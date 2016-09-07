c7bd489607189ec222a87647f4b621c8c441d117
Gohar Matinyan <goharmatinyan@gmail.com>
2016-09-07 02:00:30 -0700

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,30 @@
+//var person = {
+//	name: 'Gohar',
+//	age: 21
+//};
+
+//function updatePerson (obj) {
+	//obj = {
+	//	name: 'Gohar',
+	//	age: 31
+	//};
+//	obj.age = 31
+//}
+
+//updatePerson(person);
+//console.log(person);
+
+
+//Arrey Example
+
+var grades = [15, 37];
+
+function addGrade (gradesArr) {
+	grades.push(45);
+	debugger;
+
+	//gradesArr = [12, 15, 84];
+}
+
+addGrade(grades)
+console.log(grades);
