84efc3ff9b0ab118baaabde71afb341940abfa0d
Gohar Matinyan <goharmatinyan@gmail.com>
2016-09-21 15:33:47 -0700

    Hash and salt password

--- server.js
+++ server.js
@@ -50,6 +50,7 @@ app.get('/todos', function (req, res) {
 	// }
 
 	// res.json(filteredTodos);
+	pick
 });
 
 app.get('/todos/:id', function (req, res) {
@@ -179,7 +180,7 @@ app.post('/users', function (req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.create(body).then(function(user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function(e) {
 		res.status(400).json(e);
 	});
